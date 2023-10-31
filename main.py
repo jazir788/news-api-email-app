bf5e93fb3a5aa382cb4587c022cc61d6fbdc7096
jazir788 <jazirsx5@gmail.com>
2023-10-31 11:51:04 +0000

    Added email functionality, filtered to english articles, limited news to 20, added subject, added topic parameter

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import requests
+from send_email import  sendEmail
+
+topic = "tesla"
 
 api_key = "c09363f8c9de4490a35752416fed9236"
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2023-09-30&"\
-       "sortBy=publishedAt&apiKey=c09363f8c9de4490a35752416fed9236")
+url = "https://newsapi.org/v2/everything?"\
+        f"q={topic}&" \
+        "from=2023-09-30&"\
+        "sortBy=publishedAt"\
+        "&apiKey=c09363f8c9de4490a35752416fed9236&"\
+        "language=en"
 
 request = requests.get(url)
 content = request.json()
 
-for article in content["articles"]:
-    print(article["title"])
-    print(article["author"])
+body = "Subject: Today's news\n"
+for article in content["articles"][:20]:
+    title = article["title"]
+    author = article["author"]
+    news_url = article["url"]
+
+    if author is not None:
+        body = (body + title + "\n" + author + "\n" + news_url + 2*"\n")
+
+print(content["articles"])
+
+body = body.encode("utf-8")
+sendEmail(message=body)
